13b1ac22a17d4f55e351a20d56b210cc1142ee6b
UtescherIntrieri <18ianaui1999@gmail.com>
2023-12-18 22:13:36 -0300

    Task 3

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let result = document.getElementById('result')
+let searchRes = document.getElementById('searchRes')
 
 async function printData(){
-
+  
   let res = await fetch('https://swapi.dev/api/planets/')
   let {results} = await res.json()
   console.log(results);
-
+  
   results.forEach(planet => {
     let li = document.createElement('li')
     li.innerHTML = `
     <div>
-      <button>${planet.name}</button>
+    <button>${planet.name}</button>
     </div>`
     result.appendChild(li)
   })
 }
+async function searchPlanet(){
+  let search = document.getElementById('search').value
+  let res = await fetch(`https://swapi.dev/api/planets/?search=${search}`)
+  let {results} = await res.json()
+  let planet = results[0]
+
+  console.log(search)
+  console.log(planet)
+}
+
+window.onload = printData()
